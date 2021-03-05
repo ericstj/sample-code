9f47ddabae6df3bed04e2fa5805021a80ce73eeb
Eric StJohn <ericstj@microsoft.com>
2021-03-05 12:26:10 -0800

    Hacky sample to get native files in deps file

---
+++ addNative/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace addNative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Bar();
+            }
+            catch (System.Exception e)
+            {
+                // FileNotFoundException means it's not working.  BadImageFormatException means it worked
+                Console.WriteLine(e);
+            }
+        }
+
+        [DllImport("foo")]
+        public static extern void Bar();
+    }
+}
