ca6513a4fbeea62621a7785c084ba9abd42d6736
Eric StJohn <ericstj@microsoft.com>
2021-04-30 13:12:01 -0700

    Simple sample illustrating localization

---
+++ loc-sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace loc_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var culture = new CultureInfo(args[0]);
+                Thread.CurrentThread.CurrentUICulture = culture;
+            }
+            Console.WriteLine(resources.HelloMessage);
+        }
+    }
+}
