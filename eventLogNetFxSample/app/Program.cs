49ed934e8b3bfb47a5a342f8fd8a2c727689933d
Eric StJohn <ericstj@microsoft.com>
2021-05-19 12:56:56 -0700

    Sample demonstrating why package dependencies are needed even for facades

---
+++ eventLogNetFxSample/app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using lib;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var user in EventLogHelper.GetEventUsers())
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
---
+++ eventLogNetFxSample/lib/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+
+namespace lib
+{
+    public static class EventLogHelper
+    {
+       public static IEnumerable<string> GetEventUsers()
+       {
+            using (var eventLog = new EventLogReader("Application"))
+            {
+                while(true)
+                {
+                    using (EventRecord record = eventLog.ReadEvent())
+                    {
+                        if (record == null)
+                            break;
+
+                        yield return record.UserId?.ToString();
+                    }
+                }
+            }
+
+       }
+    }
+}
