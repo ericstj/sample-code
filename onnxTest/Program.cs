5c4dfaeb123205b13c2048fb090c0e709ec65aa5
Eric StJohn <ericstj@microsoft.com>
2021-10-14 12:55:06 -0700

    Add NETFramework sample that forces the local copy of onnxruntime

---
+++ onnxTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+
+
+namespace onnxTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var session = new InferenceSession("path to a valid onnx model", new SessionOptions
+            {
+                LogSeverityLevel = OrtLoggingLevel.ORT_LOGGING_LEVEL_ERROR
+            });
+        }
+    }
+}
