ba137fdc04997380f444b524718a5dbf0665e071
Eric StJohn <ericstj@microsoft.com>
2021-11-17 09:24:04 -0800

    Create repro for dotnet/runtime#61737

---
+++ jsonRepro61737/console/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassLibrary1;
+
+MyModel model1 = new MyModel();
+
+string json1 = Serializer.Serialize(model1);
+string json2 = Serializer.SerializeWithGenerics(model1);
+
+Console.WriteLine(json1);
+Console.WriteLine(json2);
+Console.ReadKey();
---
+++ jsonRepro61737/lib/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ClassLibrary1
+{
+    public class MyModel
+    {
+        public string Value
+        {
+            get; set;
+        }
+    }
+
+    public partial class Serializer
+    {
+        public static string Serialize(MyModel model)
+        {
+            return JsonSerializer.Serialize(
+                model,
+                SerializerContext.Default.MyModel);
+        }
+
+        public static string SerializeWithGenerics<T>(T model)
+        {
+            return JsonSerializer.Serialize(
+                model,
+                typeof(T),
+                SerializerContext.Default);
+        }
+    }
+
+    [JsonSerializable(typeof(MyModel))]
+    internal partial class SerializerContext : JsonSerializerContext
+    {
+    }
+}
