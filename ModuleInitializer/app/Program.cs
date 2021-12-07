2af9e2f4fb300eafb8d8ad87a5b0d1a694dc764c
Eric StJohn <ericstj@microsoft.com>
2021-12-07 12:02:07 -0800

    ModuleInitializer sample

---
+++ ModuleInitializer/app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+// See https://aka.ms/new-console-template for more information
+var testMod = Assembly.LoadFile(Path.Combine(AppContext.BaseDirectory, "module.dll"));
+
+Console.WriteLine($"loaded {testMod}");
+
+var type = testMod.GetExportedTypes().Single();
+
+Console.WriteLine($"got {type}");
+
+var method = type.GetMethod("Test");
+
+Console.WriteLine($"got {method}");
+
+method.Invoke(null, null);
---
+++ ModuleInitializer/module/module.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Runtime.CompilerServices;
+namespace module;
+internal class Mod
+{
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        Console.WriteLine("Initialized");
+    }
+}
+
+public class TestClass
+{
+    public static void Test() => Console.WriteLine("Test");
+}
