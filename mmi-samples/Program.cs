1833b246c21a7b4d4342a64ee91c1904ae12aaaa
Eric StJohn <ericstj@microsoft.com>
2022-03-09 15:19:50 -0800

    Add sample for using MMI

---
+++ mmi-samples/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Management.Infrastructure;
+
+ulong sizeInKB = 0;         
+EnumInstances("SELECT * FROM Win32_PhysicalMemory", instance => sizeInKB += Convert.ToUInt64(instance.CimInstanceProperties["Capacity"].Value) / 1024);
+Console.WriteLine(sizeInKB);
+
+EnumInstances("SELECT * FROM Win32_OperatingSystem", os => Console.WriteLine($"Last boot time: {os.CimInstanceProperties["LastBootUpTime"].Value}"));
+
+
+void EnumInstances(string query, Action<CimInstance> action)
+{
+    using var session = CimSession.Create(null);   
+    foreach (CimInstance instance in session.QueryInstances(@"root\cimv2", "WQL", query))
+    {
+        using (instance)
+        {
+            action(instance);
+        }
+    }
+}
