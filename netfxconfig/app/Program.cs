2417b629cc9aef666d5ee33a15178d578da39ed0
Eric StJohn <ericstj@microsoft.com>
2022-12-08 12:37:02 -0800

    ConfigurationManager misuse repro

---
+++ netfxconfig/app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lib.Class1.GetSetting());
+        }
+    }
+}
---
+++ netfxconfig/lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Configuration;
+
+namespace lib
+{
+    public class Class1
+    {
+        // this will use configurationmanager through the portable ConfigurationManager assembly.  It expects that assembly to be a facade assembly to redirect to System.dll on .NETFramework
+        public static string GetSetting() => ConfigurationManager.AppSettings.Get("LibSetting");
+    }
+}
