e5cef340e09427ab1aa6bbe4924cd8379b2b5134
Eric StJohn <ericstj@microsoft.com>
2023-07-24 08:53:01 -0700

    Add repro project

---
+++ depcontext/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyModel;
+
+var ctx= DependencyContext.Default;
+
+Console.WriteLine($"Runtime libraries: {ctx.RuntimeLibraries.Count}");
+Console.WriteLine($"Duplicates: {ctx.RuntimeLibraries.Count - ctx.RuntimeLibraries.Distinct().Count()}");
+
+foreach (var type in  ctx.RuntimeLibraries.GroupBy(l => l.Type))
+{
+    Console.WriteLine($"Type {type.Key}: {type.Count()}");
+}
---
+++ lib/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace lib;
+
+public class Class1
+{
+
+}
