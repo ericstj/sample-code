1aa7312429e424f87dc527886327681de97f9c2c
Eric StJohn <ericstj@microsoft.com>
2025-03-19 11:49:09 -0700

    Add repro for dotnet/maintenance-packages#213

---
+++ valueTupleRepro/NewVersion-fix2/libNet462/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Class1
+{
+    public static (string, string) GetVersion()
+    {
+        return (Environment.Version.ToString(), Environment.OSVersion.ToString());
+    }
+
+}
